3a22940d6d822c28bcbdca89409b35d9aec98fb8
holmofy <hff1996723@163.com>
2021-05-25 18:28:31 +0800

    update css

--- layout/_partial/after-footer.jsx
+++ layout/_partial/after-footer.jsx
@@ -5,12 +5,12 @@ const Fancybox = require('./fancybox');
 
 module.exports = class extends Component {
     render() {
-        const { config, page, theme, url_for } = this.props;
+        const { index, theme, url_for } = this.props;
         const { vendors, valine } = theme;
         const jquerySrc = vendors.jquery ? vendors.jquery : url_for('/js/jquery-3.4.1.min.js');
         return <Fragment>
             <script src={jquerySrc}></script>
-            {valine.enable ? <script dangerouslySetInnerHTML={{__html:`new Valine(${JSON.stringify({ el: '#valine-comments', ...valine})})`}}></script> : null}
+            {!index && valine.enable ? <script dangerouslySetInnerHTML={{__html:`new Valine(${JSON.stringify({ el: '#valine-comments', ...valine})})`}}></script> : null}
             <Fancybox {...this.props} />
             <script type="text/javascript" src={url_for('/js/script.js')}></script>
             <GaugesAnalytics {...this.props} />
