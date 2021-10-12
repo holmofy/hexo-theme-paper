93d47a3a6ce3c09afe8eaa49778ad432f3d9d24c
holmofy <hff1996723@gmail.com>
2021-10-12 18:24:46 +0800

    some commit

--- layout/_partial/google-analytics.jsx
+++ layout/_partial/google-analytics.jsx
@@ -5,14 +5,15 @@ module.exports = class extends Component {
         if (!this.props.theme.google_analytics) {
             return null;
         }
-        return <script type="text/javascript" dangerouslySetInnerHTML={{
-            __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', '${this.props.theme.google_analytics}', 'auto');
-ga('send', 'pageview');`
-        }}></script>;
+        const script = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-H58NSPXYPF');
+        `;
+        return <Fragment>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-H58NSPXYPF"></script>
+            <script dangerouslySetInnerHTML={{ __html: script }}></script>
+        </Fragment>;
     }
 }
