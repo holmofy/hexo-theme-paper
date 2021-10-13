00468f3e882363abf538437911343acacf54443a
hufeifei <hufeifei@banksteel.com>
2021-10-13 14:29:01 +0800

    some style

--- layout/_widget/ads_unit.jsx
+++ layout/_widget/ads_unit.jsx
@@ -3,7 +3,7 @@ const { Component } = require('inferno');
 module.exports = class extends Component {
     render() {
         const { theme } = this.props;
-        const google_ads_html = `
+        const google_ads1_html = `
         <ins class="adsbygoogle"
             style="display:block"
             data-ad-client="ca-pub-7111912103882824"
@@ -44,16 +44,28 @@ module.exports = class extends Component {
             });
         </script>
         `;
+        const google_ads2_html = `
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-7111912103882824"
+            data-ad-slot="9208319411"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+        `;
         return (
             <div className="widget-wrap">
                 <h3 className="widget-title">网站运营不易，帮点一下广告</h3>
                 <div class="ads-wrapper">
-                    <div class="google_ads" dangerouslySetInnerHTML={{ __html: google_ads_html }}></div>
+                    <div class="google_ads" dangerouslySetInnerHTML={{ __html: google_ads1_html }}></div>
                     <div class="baidu_ads">
                         <div dangerouslySetInnerHTML={{ __html: baidu_ads1_html }}></div>
                         <div dangerouslySetInnerHTML={{ __html: baidu_ads2_html }}></div>
                     </div>
                     <div class="baidu_mb_ads" dangerouslySetInnerHTML={{ __html: baidu_ads3_html }}></div>
+                    <div class="google_ads" dangerouslySetInnerHTML={{ __html: google_ads2_html }}></div>
                 </div>
             </div>
         );
