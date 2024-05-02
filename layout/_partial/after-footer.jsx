0776037764d1271a22ada6f9b2750f63fc0a94ad
holmofy <hff1996723@163.com>
2024-05-02 17:17:14 +0800

    Update after-footer.jsx

--- layout/_partial/after-footer.jsx
+++ layout/_partial/after-footer.jsx
@@ -8,9 +8,10 @@ module.exports = class extends Component {
         const { index, theme, url_for } = this.props;
         const { vendors, waline } = theme;
         const jquerySrc = vendors.jquery ? vendors.jquery : url_for('/js/jquery-3.4.1.min.js');
+        const walineJS = `import { init } from 'https://unpkg.com/@waline/client@v3/dist/waline.js';init(${JSON.stringify({ el: '#waline-comments', ...waline})});`;
         return <Fragment>
             <script src={jquerySrc}></script>
-            {!index && waline.enable ? <script dangerouslySetInnerHTML={{__html:`Waline(${JSON.stringify({ el: '#waline-comments', ...waline})})`}}></script> : null}
+            {!index && waline.enable ? <script type="module" dangerouslySetInnerHTML={{__html:walineJS}}></script> : null}
             <Fancybox {...this.props} />
             <script type="text/javascript" src={url_for('/js/script.js')}></script>
             <GaugesAnalytics {...this.props} />
