aa70aec9f729605c958147323a40f965b67fe5e3
holmofy <hff1996723@163.com>
2024-05-02 22:39:48 +0800

    Update after-footer.jsx

--- layout/_partial/after-footer.jsx
+++ layout/_partial/after-footer.jsx
@@ -5,14 +5,10 @@ const Fancybox = require('./fancybox');
 
 module.exports = class extends Component {
     render() {
-        const { index, theme, url_for } = this.props;
-        const { vendors, waline } = theme;
+        const { url_for } = this.props;
         const jquerySrc = vendors.jquery ? vendors.jquery : url_for('/js/jquery-3.4.1.min.js');
-        const walineJS = index?`import { pageviewCount } from 'https://unpkg.com/@waline/client@v3/dist/pageview.js';pageviewCount(${JSON.stringify({update:false,...waline})});`
-            :`import { init } from 'https://unpkg.com/@waline/client@v3/dist/waline.js';init(${JSON.stringify({ el:'#waline-comments',pageview:true, ...waline})});`;
         return <Fragment>
             <script src={jquerySrc}></script>
-            {waline.enable ? <script type="module" dangerouslySetInnerHTML={{__html:walineJS}}></script> : null}
             <Fancybox {...this.props} />
             <script type="text/javascript" src={url_for('/js/script.js')}></script>
             <GaugesAnalytics {...this.props} />
