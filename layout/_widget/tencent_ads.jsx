18824d1e26cb175cba8030ffd3c7f1705c00c9e0
holmofy <hff1996723@163.com>
2025-05-28 17:14:19 +0800

    fix className

--- layout/_widget/tencent_ads.jsx
+++ layout/_widget/tencent_ads.jsx
@@ -39,17 +39,17 @@ module.exports = class extends Component {
         `;
         return (
             <div className="widget-wrap ads-banner">
-                <div class="swiper-wrapper">
+                <div className="swiper-wrapper">
                     {ads.assets.map(ad => (
-                        <a class="swiper-slide" href={ad.url} title={ad.title}>
+                        <a className="swiper-slide" href={ad.url} title={ad.title}>
                             <img src={ad.img} alt={ad.title} />
                             {ad.show_title ? <span>{ad.title}</span> : null}
                         </a>
                     ))}
                 </div>
-                <div aria-label="Previous" class="swiper-button-prev"></div>
-                <div aria-label="Next" class="swiper-button-next"></div>
-                <div class="swiper-pagination"></div>
+                <div aria-label="Previous" className="swiper-button-prev"></div>
+                <div aria-label="Next" className="swiper-button-next"></div>
+                <div className="swiper-pagination"></div>
                 <script dangerouslySetInnerHTML={{ __html: scripts }}></script>
             </div>
         );
